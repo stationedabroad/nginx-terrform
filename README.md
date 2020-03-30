4a41b6b9e9e0bb8d26b3633ac707b317b434fcf1
Sulman <sulmanmh@gmail.com>
2020-03-30 16:58:25 +0100

    base terraform build of nginx

---
+++ README.md
@@ -0,0 +1,5 @@
+## to run terrform infrastructure build
+
+- terraform plan -out config.tfplan
+- terraform apply
+- terraform show (docker ps in this case also)
---
+++ config.tf
@@ -0,0 +1,20 @@
+provider = "docker" {
+	host = "tcp://docker:2345/"
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:1.11-alpine"
+}
+
+resource "docker_container" "nginx-server" {
+  name = "nginx-server"
+  image = "${docker_image.nginx.latest}"
+  ports {
+    internal = 80
+  }
+  volumes {
+    container_path  = "/usr/share/nginx/tmp"
+    host_path = "/home/stationedabroad/Documents/terraform/nginx/server-1"
+    read_only = true
+  }
+}
