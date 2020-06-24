384c1a04756d4de80646dac0d1e52c7a54272eee
sulman shaikh <sulmanmh@gmail.com>
2020-06-24 23:01:53 +0100

    added name to resource due to compile config

--- aws_main/main.tf
+++ aws_main/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 	region = "eu-west-2"
 }
 
-resource "aws_instance" {
+resource "aws_instance" "ec2_ubuntu_t2micro" {
 	ami = "ami-00f6a0c18edb19300"
 	instance_type = "t2.micro"
 }
