7967f7f4f757b6da8c9531dde3db7ba27626780d
sulman shaikh <sulmanmh@gmail.com>
2020-06-25 00:13:33 +0100

    added tags attrs for ec2 instance

--- aws_main/main.tf
+++ aws_main/main.tf
@@ -2,7 +2,11 @@ provider "aws" {
 	region = "eu-west-2"
 }
 
-resource "aws_instance" "ec2_ubuntu_t2micro" {
+resource "aws_instance" "ec2-ubuntu-t2micro" {
 	ami = "ami-00f6a0c18edb19300"
 	instance_type = "t2.micro"
+
+	tags = {
+		Name = "terraform-main-example"
+	}
 }
