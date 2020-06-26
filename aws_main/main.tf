30ed0ea9654858f6e2b097e1ebc4add0a92f75ef
sulman shaikh <sulmanmh@gmail.com>
2020-06-26 18:17:44 +0100

    added ubuntu busybox server script with security group

--- aws_main/main.tf
+++ aws_main/main.tf
@@ -2,9 +2,28 @@ provider "aws" {
 	region = "eu-west-2"
 }
 
+
+resource "aws_security_group" "instance" {
+	name = "terraform-base-ami-instance"
+
+	ingress {
+		from_port = 8080
+		to_port = 8080
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
+
 resource "aws_instance" "ec2-ubuntu-t2micro" {
 	ami = "ami-00f6a0c18edb19300"
 	instance_type = "t2.micro"
+	vpc_security_group_ids = [aws_security_group.instance.id]
+
+	user_data = <<-EOF
+			!#/bin/bash
+			echo "Server, connected status ..." > index.html
+			nohup busybox httpd -f -p 8080 &
+			EOF
 
 	tags = {
 		Name = "terraform-main-example"
