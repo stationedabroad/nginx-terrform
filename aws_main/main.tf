41a6dc42c179843cbc2f172cf9ccf3648b726e37
sulman shaikh <sulmanmh@gmail.com>
2020-06-26 19:14:31 +0100

    additional aws security group ingress added for ssh

--- aws_main/main.tf
+++ aws_main/main.tf
@@ -12,12 +12,20 @@ resource "aws_security_group" "instance" {
 		protocol = "tcp"
 		cidr_blocks = ["0.0.0.0/0"]
 	}
+
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
 }
 
 resource "aws_instance" "ec2-ubuntu-t2micro" {
 	ami = "ami-00f6a0c18edb19300"
 	instance_type = "t2.micro"
 	vpc_security_group_ids = [aws_security_group.instance.id]
+	key_name = "my-terraform"
 
 	user_data = <<-EOF
 			!#/bin/bash
