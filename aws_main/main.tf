db276dbf7b30eaea4244adf7c26b842ea6ff8c44
sulman shaikh <sulmanmh@gmail.com>
2020-06-30 23:11:15 +0100

    added defautl fixed response on LB listener - default 404 msg

--- aws_main/main.tf
+++ aws_main/main.tf
@@ -92,4 +92,14 @@ resource "aws_lb_listener" "http" {
 	load_balancer_arn = aws_lb.ec2-alb.arn
 	port = 80
 	protocol = "HTTP"
+
+	default_action {
+		type = "fixed-response"
+
+		fixed_response {
+			content_type = "text/plain"
+			message_body = "404: page not found"
+			status_code = 404
+		}
+	}
 }
